1eac817f9b728699456760d0c8763d0d13515756
Fath Amada <faturamada@gmail.com>
2020-06-01 11:49:52 +0700

    added click action

--- app/src/main/java/com/example/mopariwitara/MainDescript.java
+++ app/src/main/java/com/example/mopariwitara/MainDescript.java
@@ -1,6 +1,18 @@
 package com.example.mopariwitara;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainDescript extends AppCompatActivity {
-}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maindesc);
+
+
+        }
+};
--- app/src/main/java/com/example/mopariwitara/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mopariwitara/RecyclerViewAdapter.java
@@ -1,12 +1,14 @@
 package com.example.mopariwitara;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -30,8 +32,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public void onBindViewHolder(@NonNull ImageViewHolder imageViewHolder, int i) {
-
         imageViewHolder.img.setImageResource(mdata.get(i).getImg());
+
+        imageViewHolder.mainLayout.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, MainDescript.class);
+                mContext.startActivity(intent);
+            }
+        });
+
 
     }
 
@@ -43,9 +53,12 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public class ImageViewHolder extends RecyclerView.ViewHolder{
 
         ImageView img;
+        ConstraintLayout mainLayout;
+
         public ImageViewHolder(@NonNull View itemView) {
             super(itemView);
             img = itemView.findViewById(R.id.row_img);
+            mainLayout =itemView.findViewById(R.id.mainLayout);
         }
     }
 
